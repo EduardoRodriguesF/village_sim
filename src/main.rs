752a93b8cf379786ee76cb0bbfc8c7d2754059f8
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-12-24 15:48:32 -0300

    feat: setup better window details and camera position to align with map

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ mod prelude {
     pub use bevy::prelude::*;
 
     pub const SCALE: f32 = 1.;
+    pub const SCREEN_WIDTH: i32 = 1000;
+    pub const SCREEN_HEIGHT: i32 = 602;
 }
 
 use destinations::DestinationsPlugin;
@@ -18,7 +20,15 @@ use world::WorldPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: SCREEN_WIDTH as f32,
+                height: SCREEN_HEIGHT as f32,
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
         .add_plugin(HeadlessPositionPlugin)
         .add_plugin(WorldPlugin)
         .add_plugin(MovementPlugin)
@@ -28,5 +38,8 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz((SCREEN_WIDTH / 2) as f32, (SCREEN_HEIGHT / 2) as f32, 1.),
+        ..default()
+    });
 }
