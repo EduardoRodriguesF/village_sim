b06fe31bc589659e1a8001f846bfefc3b348d154
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-12-28 09:13:48 -0300

    feat: add time relative change by step
    Pressing `,` will slow down by 0.1, and `.` increase by the same amount

--- src/dev_tools/time_manipulation/mod.rs
+++ src/dev_tools/time_manipulation/mod.rs
@@ -9,16 +9,18 @@ impl Plugin for TimeManipulationPlugin {
 }
 
 fn control_relative_time(mut time: ResMut<Time>, keys: Res<Input<KeyCode>>) {
-    let possible_keys = vec![
-        KeyCode::Key0,
-        KeyCode::Key1,
-        KeyCode::Key2,
-        KeyCode::Key3,
-    ];
+    let possible_keys = vec![KeyCode::Key0, KeyCode::Key1, KeyCode::Key2, KeyCode::Key3];
 
     for (idx, key) in possible_keys.iter().enumerate() {
         if keys.just_pressed(*key) {
             time.set_relative_speed(idx as f32);
         }
+    }
+
+    let current = time.relative_speed();
+    if keys.just_pressed(KeyCode::Period) {
+        time.set_relative_speed(current + 0.1);
+    } else if keys.just_pressed(KeyCode::Comma) {
+        time.set_relative_speed((current - 0.1).max(0.));
     }
 }
