d72b28fa945fe973cc0324c8659b19865db99c0f
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-12-28 08:49:31 -0300

    feat: consider delta_seconds before applying velocity

--- src/movement/systems.rs
+++ src/movement/systems.rs
@@ -2,10 +2,12 @@ use super::prelude::*;
 use crate::headless_transform::components::*;
 use crate::world::prelude::*;
 
-pub fn apply_velocity(mut query: Query<(&mut HeadlessTransform, &Velocity)>) {
+pub fn apply_velocity(time: Res<Time>, mut query: Query<(&mut HeadlessTransform, &Velocity)>) {
+    let delta = time.delta_seconds() * 60.;
+
     for (mut transform, velocity) in query.iter_mut() {
-        transform.translation.x += velocity.x;
-        transform.translation.y += velocity.y;
+        transform.translation.x += velocity.x * delta;
+        transform.translation.y += velocity.y * delta;
     }
 }
 
