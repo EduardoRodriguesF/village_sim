3366733a63a7190df04800fb9534469baab077d9
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-12-28 08:51:29 -0300

    refactor: extract magic number to FPS constant

--- src/movement/systems.rs
+++ src/movement/systems.rs
@@ -1,9 +1,10 @@
 use super::prelude::*;
 use crate::headless_transform::components::*;
 use crate::world::prelude::*;
+use crate::FPS;
 
 pub fn apply_velocity(time: Res<Time>, mut query: Query<(&mut HeadlessTransform, &Velocity)>) {
-    let delta = time.delta_seconds() * 60.;
+    let delta = time.delta_seconds() * FPS;
 
     for (mut transform, velocity) in query.iter_mut() {
         transform.translation.x += velocity.x * delta;
