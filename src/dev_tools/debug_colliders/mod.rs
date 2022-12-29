27044bb56710ef060b9d1bdd3965ce5d913bc7a0
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-12-29 11:02:53 -0300

    refactor: create shapes utils to make method reusable

--- src/dev_tools/debug_colliders/mod.rs
+++ src/dev_tools/debug_colliders/mod.rs
@@ -16,27 +16,7 @@ impl Plugin for DebugCollidersPlugin {
 fn draw_colliders(mut lines: ResMut<DebugLines>, q_colliders: Query<(&HeadlessTransform, &Collider)>) {
     for (transform, collider) in q_colliders.iter() {
         let pos = to_vec2(&transform.translation);
-        draw_rectangle(&mut lines, &pos, &collider.size);
+        draw_rectangle(&mut lines, &pos, &collider.size, BOX_COLOR);
     }
 }
 
-fn draw_rectangle(lines: &mut DebugLines, pos: &Vec2, size: &Vec2) {
-    let mut draw = |start: Vec2, end: Vec2| {
-        lines.line_colored(to_vec3(&start), to_vec3(&end), 0., BOX_COLOR);
-    };
-
-    let left = pos.x;
-    let top = pos.y;
-    let right = pos.x + size.y;
-    let bottom = pos.y + size.y;
-
-    let top_left = Vec2::new(left, top);
-    let top_right = Vec2::new(right, top);
-    let bottom_left = Vec2::new(left, bottom);
-    let bottom_right = Vec2::new(right, bottom);
-
-    draw(top_left, top_right); // Top
-    draw(top_right, bottom_right); // Right
-    draw(bottom_right, bottom_left); // Bottom
-    draw(bottom_left, top_left); // Left
-}
