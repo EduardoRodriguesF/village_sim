d0c219413d7815ec8a535b5cecf805a6793b75eb
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-01-04 18:56:08 -0300

    feat(dev_tools): add manual change system

--- src/dev_tools/weather_tools/mod.rs
+++ src/dev_tools/weather_tools/mod.rs
@@ -4,12 +4,21 @@ pub struct WeatherToolsPlugin;
 
 impl Plugin for WeatherToolsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(notify_weather_change);
+        app.add_system(notify_weather_change)
+            .add_system(manual_weather_change);
     }
 }
 
 fn notify_weather_change(weather: Res<Weather>) {
     if weather.is_changed() {
         info!("Weather change: {:?}", weather.into_inner())
+    }
+}
+
+fn manual_weather_change(mut weather: ResMut<Weather>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::PageUp) {
+        weather.intensify();
+    } else if keys.just_pressed(KeyCode::PageDown) {
+        weather.lessen();
     }
 }
