00d44adf0cf99ee10c839ced89a6b116a0d49059
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-01-10 09:01:00 -0300

    feat: add splat helper methods

--- src/essentials/headless_transform/resources.rs
+++ src/essentials/headless_transform/resources.rs
@@ -3,6 +3,16 @@ use bevy::prelude::*;
 #[derive(Resource, Debug, Deref, DerefMut)]
 pub struct PixelScale(pub f32);
 
+impl PixelScale {
+    pub fn splat(&self) -> Vec2 {
+        Vec2::splat(**self)
+    }
+
+    pub fn extended_splat(&self) -> Vec3 {
+        self.splat().extend(1.)
+    }
+}
+
 impl Default for PixelScale {
     fn default() -> Self {
         Self(1.)
--- src/essentials/headless_transform/systems.rs
+++ src/essentials/headless_transform/systems.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 use bevy::prelude::*;
 
 pub fn translate_transform(scale: Res<PixelScale>, mut query: Query<(&HeadlessTransform, &mut Transform)>) {
-    let scale = Vec3::splat(**scale);
+    let scale = scale.extended_splat();
 
     for (headless, mut transform) in query.iter_mut() {
         transform.translation = (headless.translation * scale).round();
