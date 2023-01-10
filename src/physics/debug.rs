579b089340592adf61e8c9c0e479f9bf99c9c37e
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-01-10 09:04:04 -0300

    fix(physics/debug): colliders size not reflecting scale

--- src/physics/debug.rs
+++ src/physics/debug.rs
@@ -7,10 +7,15 @@ const BOX_COLOR: Color = Color::RED;
 
 pub fn draw_colliders(
     mut lines: ResMut<DebugLines>,
-    q_colliders: Query<(&HeadlessTransform, &Collider)>,
+    scale: Res<PixelScale>,
+    q_colliders: Query<(&Transform, &Collider)>,
 ) {
+    let scale = scale.splat();
+
     for (transform, collider) in q_colliders.iter() {
+        let size = collider.size * scale;
         let pos = transform.translation.truncate() + collider.offset();
-        draw_rectangle(&mut lines, &pos, &collider.size, BOX_COLOR);
+
+        draw_rectangle(&mut lines, &pos, &size, BOX_COLOR);
     }
 }
