3de322d137406e3c71fa5c6a729e1db68e7ad8fa
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-01-11 08:17:52 -0300

    feat(utils): add approach method

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,20 @@
 pub mod prelude;
 pub mod shapes;
+
+pub fn approach(mut from: f32, to: f32, amount: f32) -> f32 {
+    if from < to {
+        from += amount;
+
+        if from > to {
+            return to;
+        }
+    } else {
+        from -= amount;
+
+        if from < to {
+            return to;
+        }
+    }
+
+    from
+}
